3d8696de75b50770051326f4f64c283bcacc644f
Nick Chisiu <chisiu.n@gmail.com>
2017-04-27 09:20:46 +0300

    initial docs

--- .github/PULL_REQUEST_TEMPLATE.md
+++
@@ -1,7 +0,0 @@
-#### Short description of what this resolves:
-
-
-#### Changes proposed in this pull request:
-
-* Change 1...
-* Change 2...
--- cli/cli.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const CommandLineInterface = require('./interface');
-
-const CLI = new CommandLineInterface();
-
-CLI.load();
-
-module.exports = CLI;
